b0c91b8da4690fcf45a7b6cf089fd9debc61576f
Ariel <ariel.morales.glez@gmail.com>
2018-07-05 13:51:49 -0500

    Update_V_0_4

--- Codigo/viajes-claros/app/app.modules.js
+++
@@ -1,3 +0,0 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ngResource', 'smart-table', 'ui.bootstrap', 
-    'countTo', 'leaflet-directive', 'ngSanitize', 'ngCsv', 'chart.js', 'smoothScroll']);
-
